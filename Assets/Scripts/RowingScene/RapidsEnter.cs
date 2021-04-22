4eebaa34adcd20f136d86e1b2aa763e4b2b5004f
SandSlinky <SandSlinky@users.noreply.github.com>
2021-04-22 23:39:05 +0200

    Fixed synchronization (hopefully)

---
+++ Assets/Scripts/RowingScene/RapidsEnter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RapidsEnter : MonoBehaviour
+{
+    [SerializeField] private RowingMovement rowingMovement;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        rowingMovement.moveSpeed = 10f;
+    }
+}
